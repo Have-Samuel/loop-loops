caafb61456f18e8658ad207acedafea59bfa8142
samuel <samiehave9@gmail.com>
2024-07-16 05:39:46 +0300

    Studying Recursion

--- call_stack_underRecursion.rb
+++ call_stack_underRecursion.rb
@@ -7,3 +7,29 @@ def second
     puts 'Method2'
 end
 second
+
+def doubler(start)
+    puts start * 2
+end
+doubler(2)
+doubler(4)
+doubler(6)
+doubler(8)
+
+# Now using Recursion
+def doubler(start)
+    puts start
+    if start < 10
+        # start = (1,2,3,4) 
+        # strat = (2,4,6,8) 
+        doubler(start * 2)
+    end
+end
+doubler(2)
+
+
+
+
+
+
+
